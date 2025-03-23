cd67fe275b5de0e9fe251a801c096efc67b8a08d
rahulsinghpharthyal <rahulpharthyal04@gmail.com>
2025-03-23 10:57:52 +0530

    remove dot env packeage add inbuilt node js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,5 +1,3 @@
-import dotenv from 'dotenv';
-dotenv.config();
 
 import mysql from "mysql2/promise";
 const db = mysql.createPool({
--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 import express from "express";
 import cors from "cors";
 import db from "./database/dbConnection.js";
